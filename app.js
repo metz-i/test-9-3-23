26227f52d8808a40db2ce1f86762e55a7a569caf
Metz I <metz.imai@gmail.com>
2023-09-03 07:55:24 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log(3+4, "hello")# test-vscode
-# testing-vscode
+if (1 !== 1) {
+    console.log('tis true')
+}
+
+// example 2
+
+let rating = 5
+if (rating === 5) {
+    console.log('your driver is a platinum driver')
+}
+
