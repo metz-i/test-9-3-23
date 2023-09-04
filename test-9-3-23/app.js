c68d0ca249f709f85b2a5a8ad6037c23704290d0
Metz I <metz.imai@gmail.com>
2023-09-04 16:20:02 -0400

    array methods like push, pop, shift, and unshift

---
+++ test-9-3-23/app.js
@@ -0,0 +1,28 @@
+//let shoppingList = ['ham', 'milk', 'eggs']
+//let lotto = [45, 12, 23, 25, 34]
+//let myCollection = [12, 'dog', true, null, NaN]
+// without console.log() you can still view by calling the var within the JS console in browser
+
+let topSongs = [
+    'yay',
+    'ok',
+    'purr'
+]
+
+topSongs.push('so')
+// adding 'so' to the end of the list 
+
+dishes.unshift('fork', 'knife')
+// 2
+
+dishes
+// ['fork', 'knife']
+
+dishes.unshift('small spoon')
+// 3
+
+dishes.unshift('large spoon')
+// 4
+
+dishes
+// ['large spoon', 'small spoon', 'fork', 'knife']
